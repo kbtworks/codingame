b207ae0b0689a859d271e6430155f4af603747cc
kbtworks <kulcsarbela@gmail.com>
2020-07-02 21:38:01 +0200

    Create temperatures.py

---
+++ temperatures.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+closest = 5526
+
+for i in input().split():
+    # t: a temperature expressed as an integer ranging from -273 to 5526
+    t = int(i)
+    if abs(t) < abs(closest):
+        closest = t
+    elif abs(t) == abs(closest) and closest < t:
+        closest = abs(t)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+if n == 0:
+    print(0)
+else:
+    print(closest)
